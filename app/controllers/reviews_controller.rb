e35193c0454908f2dab1feb6224f5530a8c9bd0b
github username <github email address>
2019-04-15 00:57:38 -0700

    Adding new review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,12 +7,12 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @house = House.find_by_id(params[:house_id])
   end
 
   def create
-    binding.pry
     @review = Review.new(review_params)
-
+    binding.pry
     if @review.save
       redirect_to @review
     else
@@ -29,6 +29,6 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:cleanliness_rating, :location_rating, :value_rating, :comments)
+    params.require(:review).permit(:cleanliness_rating, :location_rating, :value_rating, :comments, :user_id, :house_id)
   end
 end
