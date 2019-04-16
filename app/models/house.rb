0d3990699e70ae80aa66625f79be5fcb825aefe7
github username <github email address>
2019-04-16 13:29:04 -0700

    Create some scope

--- app/models/house.rb
+++ app/models/house.rb
@@ -21,5 +21,35 @@ class House < ApplicationRecord
     self.reviews.average(:value_rating)
   end
 
+  def overall_rating
+    ratings = [cleanliness_rating, location_rating, value_rating]
+    overall = ratings.inject(0.0) { |sum, el| sum + el } / ratings.size
+    overall.round
+  end
+
+  def sort_by_price_per_night_asc
+    self.order(:price_per_night)
+  end
+
+  def sort_by_price_per_night_desc
+    self.reverse_order(:price_per_night)
+  end
+
+  def pets_ok
+    self.where("pets_allowed = ?", true)
+  end
+
+  def mininum_guest(guests)
+    self.where("max_guests = ?", params[guests])
+  end
+
+  def has_reviews
+    self.joins("reviews on reviews.house_id = id").distinct
+  end
+
+  def in_location(city)
+    self.where("city = ?", params[city])
+  end
+
 
 end
